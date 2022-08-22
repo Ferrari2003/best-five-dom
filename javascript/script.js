ed243b586501391a174ded186ccb75751d7a4e28
unknown <ferrari2003@gmail.com>
2022-08-23 01:14:35 +0600

    new added

---
+++ javascript/script.js
@@ -0,0 +1,36 @@
+document.getElementById("playerBtn1").addEventListener("click",function(){
+  const player =document.getElementById("best_player");
+  const li =document.createElement("li");
+  li.innerText ="King Of pele";
+  player.appendChild(li)
+})
+
+document.getElementById("playerBtn2").addEventListener("click",function(){
+  const player1= document.getElementById("best_player");
+  const li=document.createElement("li")
+  li.innerText ="Prince Ronaldinho";
+  player1.appendChild(li)
+})
+
+document.getElementById("playerBtn3").addEventListener("click",function(){
+  const player2= document.getElementById("best_player");
+  const li=document.createElement("li")
+  li.innerText ="KakA";
+  player2.appendChild(li)
+})
+
+document.getElementById("playerBtn4").addEventListener("click",function(){
+  const player3= document.getElementById("best_player");
+  const li=document.createElement("li")
+  li.innerText ="Neymar.jr";
+  player3.appendChild(li)
+})
+
+document.getElementById("playerBtn5").addEventListener("click",function(){
+  const player4= document.getElementById("best_player");
+  const li=document.createElement("li")
+  li.innerText ="Lionel Messi";
+  player4.appendChild(li)
+})
+
+
