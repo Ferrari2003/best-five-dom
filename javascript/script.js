ea3c3c065039f669c5dd3d14ba936ad958cf09c9
unknown <ferrari2003@gmail.com>
2022-08-23 04:14:17 +0600

    new commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,37 +1,13 @@
-// document.getElementById("playerBtn1").addEventListener("click",function(){
-//   const player =document.getElementById("best_player");
-//   const li =document.createElement("li");
-//   li.innerText ="King Of pele";
-//   player.appendChild(li)
-// })
 
-// document.getElementById("playerBtn2").addEventListener("click",function(){
-//   const player1= document.getElementById("best_player");
-//   const li=document.createElement("li")
-//   li.innerText ="Prince Ronaldinho";
-//   player1.appendChild(li)
-// })
+let selectedPlayer =0;
 
-// document.getElementById("playerBtn3").addEventListener("click",function(){
-//   const player2= document.getElementById("best_player");
-//   const li=document.createElement("li")
-//   li.innerText ="KakA";
-//   player2.appendChild(li)
-// })
+function selectedPlayer(player, select_btn2){
+  if(selectedPlayer <= 4){
+    
+  }
+}
 
-// document.getElementById("playerBtn4").addEventListener("click",function(){
-//   const player3= document.getElementById("best_player");
-//   const li=document.createElement("li")
-//   li.innerText ="Neymar.jr";
-//   player3.appendChild(li)
-// })
 
-// document.getElementById("playerBtn5").addEventListener("click",function(){
-//   const player4= document.getElementById("best_player");
-//   const li=document.createElement("li")
-//   li.innerText ="Lionel Messi";
-//   player4.appendChild(li)
-// })
 
 
 document.getElementById('select_btn2').addEventListener('click', function () {
